2ac9c8fa4cb841e454c5d7048f46474e74dc8ae6
rrodrigu71 <rrodrigu71@cuc.edu.co>
2024-09-18 09:57:02 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miImagen from "./assets/img/logoReact.png"
+
+function App() {
+
+  return (
+    <>
+      <div> 
+      <img src={miImagen} alt="logo React" />
+      <h1>hola mundo!!!</h1>
+      <p>este es mi primer proyecto en react..</p>  
+      </div>  
+    </>
+  )
+}
+
+export default App
